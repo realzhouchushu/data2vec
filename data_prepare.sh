3a9a7849f78e29ccff1d69d0536d700825aac837
realzhouchushu <zhouchushu03@gmail.com>
2025-07-25 16:42:17 +0800

    init commit 1

--- data_prepare.sh
+++
@@ -1,7 +0,0 @@
-python /opt/gpfs/home/chushu/codes/2506/test/fairseq/examples/wav2vec/wav2vec_manifest.py /opt/gpfs/home/chushu/data/librispeech/ --valid-percent 0 --dest /opt/gpfs/home/chushu/data/librispeech/ --ext flac --path-must-contain train
-
-# split=train
-# python /opt/gpfs/home/chushu/codes/2506/test/fairseq/examples/wav2vec/libri_labels.py /opt/gpfs/home/chushu/data/librispeech/train.tsv --output-dir /opt/gpfs/home/chushu/data/librispeech --output-name $split
-
-# split=valid
-# python /opt/gpfs/home/chushu/codes/2506/test/fairseq/examples/wav2vec/libri_labels.py /opt/gpfs/home/chushu/data/librispeech/valid.tsv --output-dir /opt/gpfs/home/chushu/data/librispeech --output-name $split
